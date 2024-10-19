8473cbc80efdbd5ef0a84fcfd3990b5e2356d268
WhiterGS1 <pmih.109@gmail.com>
2024-10-19 21:48:19 +0300

    testimoniale + about

--- asiiclicker/src/app/page.tsx
+++ asiiclicker/src/app/page.tsx
@@ -12,10 +12,10 @@ export default function Main() {
   const [pret_multiplier, setPret_multiplier] = useState(60);
 
   const [zanaPR, setZanaPR] = useState(0);
-  const [pret_zana, setPret_zana] = useState(10)
+  const [pret_zana, setPret_zana] = useState(10);
 
   const [spiridusRE, setSpiridusRE] = useState(0);
-  const [pret_spiridus, setPret_spiridus] = useState (20);
+  const [pret_spiridus, setPret_spiridus] = useState(20);
 
   const [bibliotecara, setBibliotecara] = useState(0);
   const [pret_bibliotecara, setPret_bibliotecara] = useState(30);
@@ -23,18 +23,29 @@ export default function Main() {
   const [elfRI, setElfRI] = useState(0);
   const [pret_elf, setPret_elf] = useState(40);
 
-
+  const [about, setAbout] = useState(false);
+  const [testimoniale, setTestimoniale] = useState(false);
 
   const handleClick = () => {
     setpuncte(puncte + multiplier);
   };
 
-  {/*ADAUGARE PERIODICA */}
+  const switchAbout = () => {
+    setAbout(!about);
+  };
+
+  const switchTestimoniale = () => {
+    setTestimoniale(!testimoniale);
+  };
+
+  {
+    /*ADAUGARE PERIODICA */
+  }
   let interval: number;
   useEffect(() => {
     if (vrajitorIT) {
       interval = window.setInterval(() => {
-        setpuncte((prevPuncte) => prevPuncte + vrajitorIT*4);
+        setpuncte((prevPuncte) => prevPuncte + vrajitorIT * 4);
       }, 1000);
     }
     return () => clearInterval(interval);
@@ -52,7 +63,7 @@ export default function Main() {
   useEffect(() => {
     if (spiridusRE) {
       interval = window.setInterval(() => {
-        setpuncte((prevPuncte) => prevPuncte + spiridusRE*2);
+        setpuncte((prevPuncte) => prevPuncte + spiridusRE * 2);
       }, 1000);
     }
     return () => clearInterval(interval);
@@ -61,7 +72,7 @@ export default function Main() {
   useEffect(() => {
     if (bibliotecara) {
       interval = window.setInterval(() => {
-        setpuncte((prevPuncte) => prevPuncte + bibliotecara*3);
+        setpuncte((prevPuncte) => prevPuncte + bibliotecara * 3);
       }, 1000);
     }
     return () => clearInterval(interval);
@@ -70,15 +81,15 @@ export default function Main() {
   useEffect(() => {
     if (elfRI) {
       interval = window.setInterval(() => {
-        setpuncte((prevPuncte) => prevPuncte + elfRI*4);
+        setpuncte((prevPuncte) => prevPuncte + elfRI * 4);
       }, 1000);
     }
     return () => clearInterval(interval);
   }, [elfRI]);
 
-
-
-  {/*BUY SECTION */}
+  {
+    /*BUY SECTION */
+  }
   const buyVrajitorIT = () => {
     if (puncte >= pret_vrajitor) {
       setpuncte(puncte - pret_vrajitor);
@@ -89,45 +100,39 @@ export default function Main() {
     }
   };
 
-  const buyZanaPR =() => {
-    if(puncte>=pret_zana){
+  const buyZanaPR = () => {
+    if (puncte >= pret_zana) {
       setpuncte(puncte - pret_zana);
-      setPret_zana(Math.ceil(pret_zana + pret_zana/10));
+      setPret_zana(Math.ceil(pret_zana + pret_zana / 10));
       setZanaPR(zanaPR + 1);
-    }
-    else
-    alert("Nu ai destule puncte!")
-  }
+    } else alert("Nu ai destule puncte!");
+  };
 
-  const buySpiridusRE =() => {
-    if(puncte>=pret_spiridus){
+  const buySpiridusRE = () => {
+    if (puncte >= pret_spiridus) {
       setpuncte(puncte - pret_spiridus);
-      setPret_spiridus(Math.ceil(pret_spiridus + pret_spiridus/10));
+      setPret_spiridus(Math.ceil(pret_spiridus + pret_spiridus / 10));
       setSpiridusRE(spiridusRE + 1);
-    }
-    else
-    alert("Nu ai destule puncte!")
-  }
+    } else alert("Nu ai destule puncte!");
+  };
 
-  const buyBibliotecara =() => {
-    if(puncte>=pret_bibliotecara){
+  const buyBibliotecara = () => {
+    if (puncte >= pret_bibliotecara) {
       setpuncte(puncte - pret_bibliotecara);
-      setPret_bibliotecara(Math.ceil(pret_bibliotecara + pret_bibliotecara/10));
+      setPret_bibliotecara(
+        Math.ceil(pret_bibliotecara + pret_bibliotecara / 10)
+      );
       setBibliotecara(bibliotecara + 1);
-    }
-    else
-    alert("Nu ai destule puncte!")
-  }
+    } else alert("Nu ai destule puncte!");
+  };
 
-  const buyElfRI =() => {
-    if(puncte >= pret_elf){
+  const buyElfRI = () => {
+    if (puncte >= pret_elf) {
       setpuncte(puncte - pret_elf);
-      setPret_elf(Math.ceil(pret_elf + pret_elf/10));
+      setPret_elf(Math.ceil(pret_elf + pret_elf / 10));
       setElfRI(elfRI + 1);
-    }
-    else
-    alert("Nu ai destule puncte!")
-  }
+    } else alert("Nu ai destule puncte!");
+  };
 
   const buyMultiplier = () => {
     if (puncte >= pret_multiplier) {
@@ -139,50 +144,118 @@ export default function Main() {
     }
   };
 
+  {
+    /*FRONT */
+  }
+  if (testimoniale == false) {
+    if (about == false) {
+      return (
+        <div className="flex flex-col text-center mt-12">
+          <h1>ASII Clicker</h1>
+          <p>Numar Puncte: {puncte}</p>
+          <div className="flex-col bg-cyan-400 w-1/3 ml-auto justify-end mt-60">
+            <button onClick={handleClick} style={buttonStyle}>
+              Click ASII!
+            </button>
+          </div>
 
+          <div className="flex flex-col w-1/3 bg-red-600 text-center mt-auto">
+            <button onClick={buyZanaPR} style={buttonStyle}>
+              Buy Zana PRM ({pret_zana})
+            </button>
 
-  {/*FRONT */}
-  return (
-    <div className="flex flex-col text-center mt-12">
-      <h1>ASII Clicker</h1>
-      <p>Numar Puncte: {puncte}</p>
-      <div className="flex-col bg-cyan-400 w-1/3 ml-auto justify-end mt-60">
-      <button onClick={handleClick} style={buttonStyle}>
-        Click ASII!
-      </button>
-      </div>
+            <button onClick={buySpiridusRE} style={buttonStyle}>
+              Buy Spiridus RE ({pret_spiridus})
+            </button>
 
-      <div className="flex flex-col w-1/3 bg-red-600 text-center mt-auto">
-        <button onClick={buyZanaPR} style={buttonStyle}>
-          Buy Zana PRM ({pret_zana})
-        </button>
+            <button onClick={buyBibliotecara} style={buttonStyle}>
+              Buy Bibliotecara PRO ({pret_bibliotecara})
+            </button>
 
-        <button onClick={buySpiridusRE} style={buttonStyle}>
-          Buy Spiridus RE ({pret_spiridus})
-        </button>
+            <button onClick={buyElfRI} style={buttonStyle}>
+              Buy Elf RI ({pret_elf})
+            </button>
 
-        <button onClick={buyBibliotecara} style={buttonStyle}>
-          Buy Bibliotecara PRO ({pret_bibliotecara})
-        </button>
+            <button onClick={buyVrajitorIT} style={buttonStyle}>
+              Buy Vrajitor IT ({pret_vrajitor})
+            </button>
 
-        <button onClick={buyElfRI} style={buttonStyle}>
-          Buy Elf RI ({pret_elf})
-        </button>
-
-        <button onClick={buyVrajitorIT} style={buttonStyle}>
-          Buy Vrajitor IT ({pret_vrajitor})
-        </button>
-
-        <button onClick={buyMultiplier} style={buttonStyle}>
-          Buy Multiplier ({pret_multiplier})
-        </button>
-      </div>
-    </div>
-  );
+            <button onClick={buyMultiplier} style={buttonStyle}>
+              Buy Multiplier ({pret_multiplier})
+            </button>
+          </div>
+          <div>
+            <button onClick={switchAbout} style={buttonStyle}>
+              About
+            </button>
+            <button onClick={switchTestimoniale} style={buttonStyle}>
+              Testimoniale
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-col flex-wrap mt-10">
+          <div className="flex flex-col justify-center font-bold text-2xl text-center">
+            <h1>About</h1>
+          </div>
+          <div className="flex flex-row flex-wrap">
+            <p>
+              Asociația Studenților Informaticieni Ieșeni reprezintă o
+              organizație non-guvernamentală, apolitică și non-profit ce are ca
+              scop promovarea voluntariatului și oferirea de oportunități
+              studenților. Activitățile asociației se adresează atât studenților
+              și profesorilor Facultății de Informatică Iași, cât și comunității
+              IT ieșene. Pe 12 martie 1990, ASII a devenit prima asociație
+              studențească înființată la Universitatea „Alexandru Ioan Cuza”.
+              Printre obiectivele asociației, se află facilitarea accesului
+              membrilor săi la diverse manifestări științifice, culturale și
+              sportive, promovarea spiritului civic în rândul studenților și
+              tinerilor în general, îmbunătăţirea pregătirii profesionale a
+              studenţilor informaticieni, încurajarea şi sprijinirea
+              iniţiativelor şi proiectelor ştiinţifice ale studenţilor de la
+              informatică, studenţilor alumni ai Facultăţii de Informatică Iaşi
+              şi a comunităţii IT.
+            </p>
+          </div>
+          <div className="flex w-full flex-col bg-red-600 rounded-lg justify-center font-bold">
+            <button onClick={switchAbout} className="flex flex-col">
+              Close
+            </button>
+          </div>
+        </div>
+      );
+    }
+  }
+  else{
+    return(
+    <div className="flex flex-col flex-wrap mt-10">
+          <div className="flex flex-col justify-center font-bold text-2xl text-center">
+            <h1>Testimoniale</h1>
+          </div>
+          <div className="flex flex-row flex-wrap">
+            <label htmlFor="userInput">Lasa un comentariu:</label>
+            <input
+      type="text"
+      id="userInput"
+      className="flex flex-col p-2 w-1/2 rounded-lg"
+      placeholder="Lasa un comentariu"
+    />
+          </div>
+          <div className="flex w-full flex-col bg-red-600 rounded-lg justify-center font-bold">
+            <button onClick={switchTestimoniale} className="flex flex-col">
+              Close
+            </button>
+          </div>
+        </div>
+    )
+  }
 }
 
-
-{/*STYLES */}
+{
+  /*STYLES */
+}
 const buttonStyle = {
   padding: "10px 20px",
   fontSize: "18px",
